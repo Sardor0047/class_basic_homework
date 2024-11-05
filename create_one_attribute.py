d841da146541ed77f192fcb12fc322da678ec176
Sardor0047 <macbookpro@MacBook-Pro-MacBook.local>
2024-11-05 16:38:15 +0500

    tugadi

--- create_one_attribute.py
+++ create_one_attribute.py
@@ -1,2 +1,8 @@
 #Create a "Person" class
 #Create an attribute "name" in the "Person" class
+class Person :
+    def __init__(self,name,age=None):
+        self.name = name
+        self.age = age
+    def get_name(self):
+        return self.name
--- create_some_objects_to_list.py
+++ create_some_objects_to_list.py
@@ -5,3 +5,7 @@ from create_one_attribute import Person
 #Create an object named "p3" whose name is "Jasur"
 
 #Add these objects to the "persons" named list
+p1 = Person('Anvar')
+p2 = Person("Shavkat")
+p3 = Person('Jasur')
+print([p1.get_name(),p2.get_name(),p3.get_name()])
